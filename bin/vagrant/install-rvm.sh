b1abf59af9dd6fb550b23bef6b25c3da9c769498
Matt Light <matt.light@lightdatasys.com>
2017-09-18 23:05:56 -0700

    Install RVM and a newer verion of Ruby
    1.9??! I don't need the newest, but I was having an issue getting newgem
    to run and figured I'd bring Ruby up-to-date in case it is useful for
    other things.

---
+++ bin/vagrant/install-rvm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if ! which rvm ; then
+  apt-get update -qq -y
+  apt-get install -qq -y software-properties-common
+
+  apt-add-repository -y ppa:rael-gc/rvm 2>&1
+  apt-get update -qq -y
+  apt-get install -qq -y rvm
+
+  usermod -aG rvm vagrant
+
+  source /etc/profile.d/rvm.sh
+
+  rvm install --quiet-curl 2.4.1
+  rvm use 2.4.1
+  gem install bundler -q
+fi
