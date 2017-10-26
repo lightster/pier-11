260fabd5230a02f0e4dec3fb065e02473f861703
Matt Light <matt.light@illuminateed.com>
2017-10-25 22:39:25 -0700

    Upgrade docker-compose
    `docker-compose top` was throwing an error about not being able to
    determine the width of the terminal. The issue was fixed in
    docker-compose 1.16.

--- bin/vagrant/install-docker-compose.sh
+++ bin/vagrant/install-docker-compose.sh
@@ -3,7 +3,7 @@
 echo -n "Installing docker-compose"
 
 if [ ! -f /usr/local/bin/docker-compose ]; then
-  curl -sS -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  curl -sS -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
   echo "docker-compose installed!"
 else
